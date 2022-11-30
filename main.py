971b6b0f3f25a9c7b3c8c06cec39abd25ca8875f
Reyn12 <99856980+Reyn12@users.noreply.github.com>
2022-11-30 17:44:49 +0700

    revisi1
    copy

--- main.py
+++ main.py
@@ -1,99 +1,185 @@
-namaBuku = "null"
-stok = 0
-jumlahBuku = 0
-status = "Tidak Aman"
-hargaBuku =  int(0)
-kodeBuku = str.upper(input('Masukkan kode : '))
-validasi = 'Y'
-
-# FISIKA
-if kodeBuku == "FSK01":
-    jumlahBuku = 1
-    kodeBuku = "FSK01"
-    namaBuku = "Fisika"
-    hargaBuku = 35000
-    hargaJual = hargaBuku+(12.5/100*hargaBuku)
-    stok = 25
-    stokAman = int(input("Masukkan stok aman : "))
-    if stokAman <= 25 :
-        status = "Stok Aman"
-        validasi = str.upper(input('Masukkan Data Lagi? [Y/T] : '))
-        if validasi == 'Y':
-            jumlahBuku  == jumlahBuku + 1
-            kodeBuku = str.upper(input('Masukkan kode : '))
-            # KIMIA
-            if kodeBuku == "KIM02":
-                namaBuku = "Kimia"
-                hargaBuku = 40000
-                hargaJual = hargaBuku+(15/100*hargaBuku)
-                stok = 40
-                stokAman = int(input("Masukkan stok aman : "))
-                if stokAman <= 40 :
-                    status = "Stok Aman"
-                    validasi = str.upper(input('Masukkan Data Lagi? [Y/T] : '))
-                    if validasi == 'Y':
-                        jumlahBuku  == jumlahBuku + 1
-                        kodeBuku = str.upper(input('Masukkan kode : '))
-                else:
-                    jumlahBuku  == jumlahBuku + 1
-                    status ="Tidak Aman"
-        else:
-            jumlahBuku  == jumlahBuku + 1
-            print('jumlah buku =', jumlahBuku)
+print(40*("-"))
+kode_buku = input('Masukkan Kode Barang   : ')
+kode_buku = kode_buku.upper()
+stok_aman = int(input('Masukkan Stok Aman     : '))
+juml_dt_buku = 0
+status_aman = 0
+fisika = 0
+kimia = 0
+biologi = 0
+matematika = 0
+indonesia = 0
+if(kode_buku == "FSK01"):
+    nama_buku  = "FISIKA"
+    harga_beli = 35000
+    harga_jual = harga_beli + (harga_beli * 0.125)
+    stok       = 25
+    fisika     = stok_aman
+    juml_dt_buku += 1  
+    if(stok_aman >= stok):
+        status_aman += 1
     else:
-        status ="Tidak Aman"
-    
-# KIMIA
-# elif kodeBuku == "KIM02":
-#     namaBuku = "Kimia"
-#     hargaBuku = 40000
-#     hargaJual = hargaBuku+(15/100*hargaBuku)
-#     stok = 40
-#     stokAman = int(input("Masukkan stok aman : "))
-#     if stokAman <= 40 :
-#         status = "Stok Aman"
-#     else:
-#         status ="Tidak Aman"
+        status_aman = 0
+elif(kode_buku == "KIM02"):
+    nama_buku   = "KIMIA"
+    harga_beli  = 40000
+    harga_jual  = harga_beli + (harga_beli * 0.15)
+    stok        = 40
+    kimia       = stok_aman
+    juml_dt_buku += 1
+    if(stok_aman >= stok):
+        status_aman += 1
+    else:
+        status_aman = 0
+elif(kode_buku == "BIG03"):
+    nama_buku   = "BIOLOGI"
+    harga_beli  = 50000
+    harga_jual  = harga_beli + (harga_beli * 0.15)
+    stok        = 10
+    biologi = stok_aman
+    juml_dt_buku += 1
+    if(stok_aman >= stok):
+        status_aman += 1
+    else:
+        status_aman = 0
+elif(kode_buku == "MTK04"):
+    nama_buku   = "MATEMATIKA"
+    harga_beli  = 45000
+    harga_jual  = harga_beli + (harga_beli * 0.125)
+    stok        = 7
+    matematika  = stok_aman
+    juml_dt_buku += 1
+    if(stok_aman >= stok):
+        status_aman += 1
+    else:
+        status_aman = 0
+elif(kode_buku == "BIN05"):
+    nama_buku   = "BAHASA INDONESIA"
+    harga_beli  = 42500
+    harga_jual  = harga_beli + (harga_beli * 0.125)
+    stok        = 22
+    indonesia = stok_aman
+    juml_dt_buku += 1
+    if(stok_aman >= stok):
+        status_aman += 1
+    else:
+        status_aman = 0
+else:
+    print("Buku Tidak Tersedia")
 
-# BIOLOGI
-# elif kodeBuku == "BIG03":
-#     namaBuku = "Biologi"
-#     hargaBuku = 50000
-#     hargaJual = hargaBuku+(15/100*hargaBuku)
-#     stok = 10 
-#     stokAman = int(input("Masukkan stok aman : "))
-#     if stokAman <= 10 :
-#         status = "Stok Aman"
-#     else:
-#         status ="Tidak Aman"
+print("Nama Barang            :",nama_buku)
+print(f'Harga Beli             : Rp { harga_beli:,}')
+print(f'Harga Jual             : Rp { harga_jual:,}')
+print("Stok                   :",stok)
+print("Stok Aman              :",stok_aman)
+if(status_aman > 0):
+    print("Status                 : Aman")
+    print(40*("-"))
+else:
+    print("Status                 : Tidak Aman")
+    print(40*("-"))
 
-# MTK
-# elif kodeBuku == "MTK04" :
-#     namaBuku = "Matematika"
-#     hargaBuku = 45000
-#     hargaJual = hargaBuku+(12.5/100*hargaBuku)
-#     stok = 7
-#     stokAman = int(input("Masukkan stok aman : "))
-#     if stokAman <= 7 :
-#         status = "Stok Aman"
-#     else:
-#         status ="Tidak Aman"
+ 
+pilihan = input("Mau Input Data Lagi Gak [Y/T] ? : ")
+print(40*("-"))
+juml_dt_buku = juml_dt_buku
+status_aman  = status_aman
+print
+while(pilihan == "Y"):
+    kode_buku = input('Masukkan Kode Barang   : ')
+    kode_buku = kode_buku.upper()
+    stok_aman   = int(input('Masukkan Stok Aman     : '))
+    if(kode_buku == "FSK01"):
+        nama_buku   = "FISIKA"
+        harga_beli  = 35000
+        harga_jual  = harga_beli + (harga_beli * 0.125)
+        stok        = 25
+        fisika = stok_aman
+        juml_dt_buku += 1  
+        if(stok_aman >= stok):
+            status_aman += 1
+        else:
+            status_aman = 0
+    elif(kode_buku == "KIM02"):
+        nama_buku   = "KIMIA"
+        harga_beli  = 40000
+        harga_jual  = harga_beli + (harga_beli * 0.15)
+        stok        = 40
+        kimia = stok_aman
+        juml_dt_buku += 1
+        if(stok_aman >= stok):
+            status_aman += 1
+        else:
+            status_aman = 0
+    elif(kode_buku == "BIG03"):
+        nama_buku   = "BIOLOGI"
+        harga_beli  = 50000
+        harga_jual  = harga_beli + (harga_beli * 0.15)
+        stok        = 10
+        biologi = stok_aman
+        juml_dt_buku += 1
+        if(stok_aman >= stok):
+            status_aman += 1
+        else:
+            status_aman = 0
+    elif(kode_buku == "MTK04"):
+        nama_buku   = "MATEMATIKA"
+        harga_beli  = 45000
+        harga_jual  = harga_beli + (harga_beli * 0.125)
+        stok        = 7
+        matematika  = stok_aman
+        juml_dt_buku   += 1
+        if(stok_aman >= stok):
+            status_aman += 1
+        else:
+            status_aman = 0
+    elif(kode_buku == "BIN05"):
+        nama_buku   = "BAHASA INDONESIA"
+        harga_beli  = 42500
+        harga_jual  = harga_beli + (harga_beli * 0.125)
+        stok        = 22
+        indonesia   = stok_aman
+        juml_dt_buku += 1
+        if(stok_aman >= stok):
+            status_aman += 1
+        else:
+            status_aman = 0
+    else:
+        print("Buku Tidak Tersedia")
 
-# BINDONESIA
-# elif kodeBuku == "BIN05":
-#     namaBuku = "Bindonesia"
-#     hargaBuku = 42500
-#     hargaJual = hargaBuku+(12.5/100*hargaBuku)
-#     stok = 22
-#     stokAman = int(input("Masukkan stok aman : "))
-#     if stokAman <= 22 :
-#         status = "Stok Aman"
-#     else:
-#         status ="Tidak Aman"
+    print("Nama Barang            :",nama_buku)
+    print(f'Harga Beli             : Rp { harga_beli:,}')
+    print(f'Harga Jual             : Rp { harga_jual:,}')
+    print("Stok                   :",stok)
+    print("Stok Aman              :",stok_aman)
+    if(status_aman > 0):
+        print("Status                 : Aman")
+        print(40*("-"))
+    else:
+        print("Status                 : Tidak Aman")
+        print(40*("-"))
+    pilihan = input("Mau Input Data Lagi Gak [Y/T] ? :")
+    print(40*("-"))
 
-else :
-    print("Kode buku salah")
-    kodeBuku = str.upper(input('Masukkan kode : '))
 
-# print("Jumlah Buku = ", jumlahBuku)
-# print("")
+if(pilihan == "T"):
+    print('Jumlah Data Buku       : ', juml_dt_buku)
+    print("Stok Aman              : ", status_aman, "Buku")
+    stok_tidak_aman = juml_dt_buku - status_aman
+    print("Stok Tidak Aman        : ", stok_tidak_aman, "Buku, yaitu Buku dengan Kode :")
+    no = 0
+    if(fisika < 25  and fisika != 0):
+        no += 1
+        print(no, ". FISIKA")
+    if(kimia < 40 and kimia != 0):
+        no += 1
+        print(no, ". KIMIA")
+    if(biologi < 10 and biologi != 0):
+        no += 1
+        print(no, ". BIOLOGI")
+    if(indonesia < 22 and indonesia != 0):
+        no += 1
+        print(no, ". BAHASA INDONESIA")
+    if(matematika < 7 and matematika != 0):
+        no += 1
+        print(no, ". MATEMATIKA")
